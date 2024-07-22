18abcb6ce6f32609707f2bfa394370ca4d184eca
Tani Diament <114248178+TaniDiament@users.noreply.github.com>
2024-07-22 11:11:03 +0300

    Update BTree.java

--- src/main/java/edu/yu/cs/com1320/project/BTree.java
+++ src/main/java/edu/yu/cs/com1320/project/BTree.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package edu.yu.cs.com1320.project;
 
 import edu.yu.cs.com1320.project.stage5.PersistenceManager;
@@ -8,7 +7,7 @@ public interface BTree<Key extends Comparable<Key>, Value> {
     Value put(Key k, Value v);
     void moveToDisk(Key k) throws Exception;
     void setPersistenceManager(PersistenceManager<Key,Value> pm);
-=======
+
 package edu.yu.cs.com1320.project;
 
 import edu.yu.cs.com1320.project.stage5.PersistenceManager;
@@ -18,5 +17,4 @@ public interface BTree<Key extends Comparable<Key>, Value> {
     Value put(Key k, Value v);
     void moveToDisk(Key k) throws Exception;
     void setPersistenceManager(PersistenceManager<Key,Value> pm);
->>>>>>> fd77063a3d4afb76b6777a38f3f83134aeed8ddf
 }
